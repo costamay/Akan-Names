bc1d75c8571599d0618fc7164460edf2c43ea4ca
Titus Ouko <titusouko@gmail.com>
2019-08-18 10:37:08 +0300

    add variable list for for male and female akan names with validation function in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -68,58 +68,53 @@ function ClearFields() {
 // }
 
 function validation() {
-    var date = document.getElementById("d");
+    var date = document.getElementById("d").value;
     
-    var month = document.getElementById("m");
-    var year = document.getElementById("yy");
+    var month = document.getElementById("m").value;
+    var year = document.getElementById("yy").value;
     var gen = document.getElementsByName("gd");
     var currentDate = new Date();
     var currentYear = currentDate.getFullYear();
-    if(isNaN(date.value) ||date.value <1 || date.value >31 ||date.value === null || date.value === ""){
-        //alert("Invalid date format");
-    document.getElementById('one').innerHTML = "invalid day";
-    document.getElementById('one').style.color = "red"
-    }else{
-        //alert("continue");
-        return date;
-    }
-    if (isNaN(month.value) || month.value < 1 || month.value > 12 || month.value === "" || month.value == null) {
-        //alert('invalid month');
-        document.getElementById('two').innerHTML = "invalid month";
-    document.getElementById('two').style.color = "red"
-    } else {
-        //alert('you ok');
-        return month;
-    }
-    if(isNaN(year.value) || year.value === "" ||year.value === null || year.value <1900 || year.value > currentYear){
-        //alert("invalid year");
-        document.getElementById('three').innerHTML = "invalid year";
-    document.getElementById('three').style.color = "red"
-    }else{
-        //alert("select gender")
-        return year;
+    var ListofDays = [31,28,31,30,31,30,31,31,30,31,30,31];
+    
+    // if(isNaN(date.value) ||date.value <1 || date.value >31 ||date.value === null || date.value === ""){
+    //     //alert("Invalid date format");
+    // document.getElementById('one').innerHTML = "invalid day";
+    // document.getElementById('one').style.color = "red"
+    // }else{
+    //     //alert("continue");
+    //     return date;
+    // }
+    // if (isNaN(month.value) || month.value < 1 || month.value > 12 || month.value === "" || month.value == null) {
+    //     //alert('invalid month');
+    //     document.getElementById('two').innerHTML = "invalid month";
+    // document.getElementById('two').style.color = "red"
+    // } else {
+    //     //alert('you ok');
+    //     return month;
+    // }
+    // if(isNaN(year.value) || year.value === "" ||year.value === null || year.value <1900 || year.value > currentYear){
+    //     //alert("invalid year");
+    //     document.getElementById('three').innerHTML = "invalid year";
+    // document.getElementById('three').style.color = "red"
+    // }else{
+    //     //alert("select gender")
+    //     return year;
         
-    }
-    if (gen[0].checked === false && gen[1].checked === false) {
-       // alert('get out of here');
-        document.getElementById('four').innerHTML = "select gender";
-    document.getElementById('four').style.color = "red"
-    } else {
-        for (i = 0; i < gen.length; i++) {
-            if (gen[i].checked)
-                var gend= gen[i].value
-                return gend;
-                }
+    // }
+    // if (gen[0].checked === false && gen[1].checked === false) {
+    //    // alert('get out of here');
+    //     document.getElementById('four').innerHTML = "select gender";
+    // document.getElementById('four').style.color = "red"
+    // } else {
+    //     for (i = 0; i < gen.length; i++) {
+    //         if (gen[i].checked)
+    //             var gend= gen[i].value
+    //             return gend;
+    //             }
 
 }
-var getDetails = {
-    myDate:date.value,
-    myMonth: month.value,
-    myYear: year.value,
-    myGender: gend
 
-}
-return getDetails;
 // function calDay(){
 //     var userInputs = validation();
 //     updateDate = myDetails.newDate
